b21f6b8729d255ad479a3776d9ae5ba91c4c3de3
J Christie <jchristie1@gmail.com>
2018-06-06 11:32:04 -0700

    Added 'else if' for no answers and an catch for all other answers per question

--- js/app.js
+++ js/app.js
@@ -2,58 +2,75 @@
 
 //  conditional logic
 //  if/else
-
+var wrongResponce = "Youve entered something other than 'yes' or 'no'";
 var eatsBread = prompt("Do you like bread? (only answer with 'yes' or 'no')").toLowerCase();
 
 //  Question 1 bread
-if(eatsBread === 'yes'){
-    console.log("Bread answer: " +eatsBread)
+if (eatsBread === 'yes') {
+    console.log("Bread answer: " + eatsBread);
     alert("YAY");
-} else{
-    console.log("Bread answer: " +eatsBread)
+} else if (eatsBread === 'no') {
+    console.log("Bread answer: " + eatsBread);
     alert('Lame');
+} else {
+    console.log("Bread answer: " + eatsBread);
+    alert(wrongResponce);
 }
 
 //  Question 2 Pizza
 var eatsPizza = prompt("Do you like Pizza? (only answer with 'yes' or 'no')").toLowerCase();
 
-if(eatsPizza === 'yes'){
-    console.log("Pizza answer: " +eatsPizza)
+if (eatsPizza === 'yes') {
+    console.log("Pizza answer: " + eatsPizza);
     alert("PIZZA");
-} else{
-    console.log("Pizza answer: " +eatsPizza)
+} else if (eatsPizza === 'no'){
+    console.log("Pizza answer: " + eatsPizza);
     alert('WHAT?');
+} else {
+    console.log("Bread answer: " + eatsPizza);
+    alert(wrongResponce);
 }
 
 //  Question 3 Dinner
 var eatsDinner = prompt("Is dinner your favorite meal? (only answer with 'yes' or 'no')").toLowerCase();
 
-if(eatsDinner === 'yes'){
-    console.log("Dinner answer: " +eatsDinner)
+if (eatsDinner === 'yes') {
+    console.log("Dinner answer: " + eatsDinner);
     alert("Lets eat dinner!");
-} else{
-    console.log("Dinner answer: " +eatsDinner)
+} else if (eatsDinner === 'no'){
+    console.log("Dinner answer: " + eatsDinner);
     alert('Lets make breakfast!');
+} else {
+    console.log("Bread answer: " + eatsDinner);
+    alert(wrongResponce);
 }
+
 
 //  Question 4 Hungry
 var eatsHungry = prompt("Are you hungry now? (only answer with 'yes' or 'no')").toLowerCase();
 
-if(eatsHungry === 'yes'){
-    console.log("Hungry answer: " +eatsHungry)
+if (eatsHungry === 'yes') {
+    console.log("Hungry answer: " + eatsHungry);
     alert("Lets get some food!");
-} else{
-    console.log("Hungry answer: " +eatsHungry)
+} else if (eatsHungry === 'no'){
+    console.log("Hungry answer: " + eatsHungry);
     alert('Lets get some food later!');
+} else {
+    console.log("Bread answer: " + eatsHungry);
+    alert(wrongResponce);
 }
+
 
 //  Question 5 Sleep
 var eatsSleep = prompt("Do you get tired after you eat a big meal? (only answer with 'yes' or 'no')").toLowerCase();
 
-if(eatsSleep === 'yes'){
-    console.log("Sleeps answer: " +eatsSleep)
+if (eatsSleep === 'yes') {
+    console.log("Sleeps answer: " + eatsSleep);
     alert("Time to digest!");
-} else{
-    console.log("Sleeps answer: " +eatsSleep)
+} else if (eatsSleep === 'no'){
+    console.log("Sleeps answer: " + eatsSleep);
     alert('Time for a jog!');
+} else {
+    console.log("Bread answer: " + eatsSleep);
+    alert(wrongResponce);
 }

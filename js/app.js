ff32ba09f6a71a05de76cd782ff6bc6e54ed3801
J Christie <jchristie1@gmail.com>
2018-06-06 23:16:48 -0700

    Work still needed to get usable functionality on the last question (iterate an array). Most of the code commented out for fast rerun testing, needs fixed!

--- js/app.js
+++ js/app.js
@@ -96,33 +96,33 @@
 
 //6 Number Question 
 
-var j = 0;
+// var j = 0;
 
-while (j++, j < 4){
+// while (j++, j < 4){
     
 
-    var numGuess = parseInt(prompt('Pick the magic number between 1 - 10 within 3 guesses: '));
+//     var numGuess = parseInt(prompt('Pick the magic number between 1 - 10 within 3 guesses: '));
 
-    if (numGuess === 5){
-        console.log('Answer: ' + numGuess + ' Correct on try: ' + j);
-        alert('Nailed it! Magic number is 5');
-        break;
-    } else if (numGuess < 5){
-        console.log('Answer: ' + numGuess + ' number of tries: ' + j)
-        alert(numGuess + ' is to low, please try again.');
-    }else if (numGuess > 5){
-        console.log('Answer: ' + numGuess + ' number of tries: ' + j)
-        alert(numGuess + ' is to high, please try again');
-    } else {
-            console.log('Answer: ' + numGuess + ' number of tries: ' + j)
-        alert('Maybe try a number?')
-    }
-    if (j === 3){
-        console.log('Maximum guesses exceeded. Loop exit.')
-        alert('Out of guesses! Correct answer is 5.')
-        break;
-    }
-}
+//     if (numGuess === 5){
+//         console.log('Answer: ' + numGuess + ' Correct on try: ' + j);
+//         alert('Nailed it! Magic number is 5');
+//         break;
+//     } else if (numGuess < 5){
+//         console.log('Answer: ' + numGuess + ' number of tries: ' + j);
+//         alert(numGuess + ' is to low, please try again.');
+//     }else if (numGuess > 5){
+//         console.log('Answer: ' + numGuess + ' number of tries: ' + j);
+//         alert(numGuess + ' is to high, please try again');
+//     } else {
+//             console.log('Answer: ' + numGuess + ' number of tries: ' + j);
+//         alert('Maybe try a number?');
+//     }
+//     if (j === 3){
+//         console.log('Maximum guesses exceeded. Loop exit.');
+//         alert('Out of guesses! Correct answer is 5.');
+//         break;
+//     }
+// }
 
 
 
@@ -132,17 +132,22 @@ while (j++, j < 4){
 //     a. limit number of guesses (6)
 //     b. provide feedback on guesses
 
-var randArr = [1, 2, 3];
+var k = 0;
+var randArr = ['red', 'blue', 'white'];
 
-var userGuess = parseInt(prompt('number 1-10'));
+var userGuess = (prompt('choose a basic color:'));
     console.log('user input: ' + userGuess);
 
-for(var x = 0; x < randArr.length; x++){
-    console.log('x referencing: ', userGuess[x]);
+for(var x = 0; x < randArr.length; x++, k++){
+
+    console.log('iteration referencing: ', x);
     if (userGuess === randArr[x]){
-        alert('that is a good number!');
+        alert('I like that color too!');
+        console.log("k count is: " + k);
         break;
-    } else{
-        alert('bad number');
-    }
+    } 
+    console.log("k count is: " + k);
+    // else{
+    //     alert('Possible color in the future');
+    // }
   }  
